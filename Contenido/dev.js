d0aef48f4183a71e14d84688380cd1403e95d749
XarlosZakk <carlosgamer1009@gmail.com>
2025-09-12 22:11:12 -0500

    cambios Nuevos

--- Contenido/dev.js
+++ Contenido/dev.js
@@ -1,1 +1,10 @@
-let iniciarCorrecion = 'coregir';
+let contenedor = document.getElementById("recipiente"); //captura de elemnto
+let btnAccion = document.getElementById("btn"); // captura de boton
+btnAccion.addEventListener("click", () => {
+  if (!contenedor.querySelector("h2")) {
+    // Solo si no existe un h2
+    let newElement = document.createElement("div");
+    newElement.innerHTML = "<h2>Hola Mundo</h2>";
+    contenedor.appendChild(newElement);
+  }
+});
